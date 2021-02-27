729f1ea915f7af4223fbb2309720b9fc18d479be
jokopurnomo <jokopurnomo0710@gmail.com>
2021-02-27 23:42:02 +0700

    first committt

--- about.php
+++ about.php
@@ -9,12 +9,12 @@ include "template/navbar.php";
     <div class="row">
         <div class="col-md-12">
             <div class="card">
-                    <h5 class="card-header text-dark">About Us</h5>
-                    <div class="card-body">
-                        <div class="row">
-                            <p class="text-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, ipsa!</p>
-                        </div>
+                <h5 class="card-header text-dark">About Us</h5>
+                <div class="card-body">
+                    <div class="row">
+                        <p class="text-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, ipsa!</p>
                     </div>
+                </div>
         </div>
     </div>
 </div>
