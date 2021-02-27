8ec042485365352bff2065bdac904a22bd5b41b3
jokopurnomo <jokopurnomo0710@gmail.com>
2021-02-27 23:40:26 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,27 @@
+<?php
+
+include "template/navbar.php";
+
+?>
+
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                    <h5 class="card-header text-dark">About Us</h5>
+                    <div class="card-body">
+                        <div class="row">
+                            <p class="text-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, ipsa!</p>
+                        </div>
+                    </div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+
+include "template/footer.php";
+
+?>
--- template/navbar.php
+++ template/navbar.php
@@ -21,7 +21,9 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">
+        <img src="/docs/4.6/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="Logo">
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
